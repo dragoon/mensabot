905aa9cee711f8188dd881b850acede7cf0f116e
Claude Bossy <cb@fairtiq.com>
2019-05-16 11:40:05 +0200

    fix joe parser with weekday check

--- menu_parsers/joes_parser.py
+++ menu_parsers/joes_parser.py
@@ -15,11 +15,13 @@ class JoesParser(MenuParser):
         html_body = requests.get("https://www.joes-popup.ch/menuplan/").text
         root = lxml.html.fromstring(html_body)
 
+        weekdays = ['MONTAG', 'DIENSTAG', 'MITTWOCH', 'DONNERSTAG', 'FREITAG', 'SAMSTAG', 'SONNTAG']
+
         menu_contents = root.xpath('//div[@class="menu_content_classic"]')
         for menu_content in menu_contents:
             menu_title = menu_content.xpath('h5/span[@class="menu_title"]')
             if len(menu_title) > 0:
-                if timestamp.strftime("%d.%m.%Y") in menu_title[0].text_content():
+                if weekdays[timestamp.weekday()] in menu_title[0].text_content():
                     menu = self.MENSA_NAME + '\n' + menu_content.xpath('div[@class="post_detail menu_excerpt"]')[0].text_content()
                     return menu
 
